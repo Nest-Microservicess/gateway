dc29b10d4a6988fcbce06aca3760e01579fa1ddc
hernanacevedo <42323463+hernanacevedo@users.noreply.github.com>
2025-01-15 11:24:19 -0300

    se agrega validacion de auth y comunicacion

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { AuthController } from './auth.controller';
+import { NatsModule } from 'src/transport/nats.module';
+
+
+@Module({
+  controllers: [AuthController],
+  imports: [NatsModule],
+})
+export class AuthModule {}
